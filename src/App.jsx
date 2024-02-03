f4c79674390a761d819f64a71038a19770c1404b
Phil <pamramfam@gmail.com>
2024-02-03 09:06:50 -0500

    added jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
-
+import { puppyList } from './data'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const handleClick = (id) => {
+    setFeatPupId(id);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => (
+        <div key={puppy.id} onClick={() => handleClick(puppy.id)}>
+          <h2>{puppy.name}</h2>
+          {featPupId && (
+            <div>
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
